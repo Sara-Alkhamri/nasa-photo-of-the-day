8fbd41ba8dcdb2a65fd96f2aba3ecfa2a20a30df
Sara Alkhamri <s.alkhamri@gmail.com>
2019-08-15 13:08:38 -0700

    imported styled components into index.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import "./App.css";
-import "./index.scss";
+import "./App.scss";
+import "./index.css";
 
 //import components
 
