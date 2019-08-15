908b457605bf92f0dcf6f29c9d9bc67849e3b92e
Sara Alkhamri <s.alkhamri@gmail.com>
2019-08-15 13:57:12 -0700

    Moved the styled compnent import to APP.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.scss";
 import "./index.css";
+import styled from "styled-components";
 
 //import components
 
